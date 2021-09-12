cebbeec0eddb61ad28c9ef2126963a020258fbae
Twilighters <ilya475@rambler.ru>
2021-09-13 01:29:04 +0300

    create db and test Flask hello_world

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hi")
+from flask import Flask, jsonify, request
+from onyx_taxi_db import Driver, Client, Order
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, world</p>"
+
+
+@app.route("/drivers", methods=["POST"])
+def post_driver():
+    content = request.get_json()
+    return jsonify(content)
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ onyx_taxi_db.py
@@ -0,0 +1,75 @@
+from sqlalchemy import Column, Integer, String, create_engine, Boolean, ForeignKey, DateTime, func
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+
+# engine - пул соединений к БД
+engine = create_engine('sqlite:///onyx_taxi.db')
+
+# declarative_base - фабричная функция, возвращающая базовый класс, от которого произойдет наследование класса с моделью.
+Base = declarative_base()
+
+
+# Класс, описывающий одну из таблиц БД. Такой класс назывется моделью.
+class Driver(Base):
+    __tablename__ = 'drivers'  # имя таблицы
+
+    # Атрибуты класса описывают колонки таблицы, их типы данных и ограничения
+    id = Column(Integer, primary_key=True, autoincrement=True, comment="Идентификатор водителя")
+    name = Column(String(25), nullable=False, comment="Имя водителя")
+    car = Column(String, comment="автомобиль водителя")
+
+    # переопределение выдачи объекта на экран через print
+    def __repr__(self):
+        return "<Player(name={0}, car={1})>".format(self.name, self.car)
+
+
+class Client(Base):
+    __tablename__ = 'clients'  # имя таблицы
+
+    # Атрибуты класса описывают колонки таблицы, их типы данных и ограничения
+    id = Column(Integer, primary_key=True, autoincrement=True, comment="Идентификатор клиента")
+    name = Column(String(25), nullable=False, comment="Имя клиента")
+    is_vip = Column(Boolean, default=False, nullable=False, comment="автомобиль водителя")
+
+    # переопределение выдачи объекта на экран через print
+    def __repr__(self):
+        return "<Client(name={0}, is_vip={1})>".format(self.name, self.is_vip)
+
+
+class Order(Base):
+    __tablename__ = 'orders'  # имя таблицы
+
+    # Атрибуты класса описывают колонки таблицы, их типы данных и ограничения
+    id = Column(Integer, primary_key=True, autoincrement=True, comment="Идентификатор заказа")
+    address_from = Column(String(50), nullable=False, comment="Адрес: Откуда")
+    address_to = Column(String(50), nullable=False, comment="Адрес: Куда")
+    client_id = Column(Integer, ForeignKey('client_id'), nullable=False, comment="Идентификатор клиента")
+    driver_id = Column(Integer, ForeignKey('driver_id'), nullable=False, comment="Идентификатор водителя")
+    date_created = Column(DateTime(timezone=True), default=func.now(), comment="Дата и время создания заказа")
+    status = Column(String(20), nullable=False, default="not_accepted", comment="Статус")
+    client = relationship("Client", foreign_keys=[client_id])
+    driver = relationship("Driver", foreign_keys=[driver_id])
+
+    # переопределение выдачи объекта на экран через print
+    def __repr__(self):
+        return "<Order(address_from={0}, address_to={1},client_id={2}, driver_id={3},date_created={4}, status={5},)>".format(
+            self.address_from,
+            self.address_to,
+            self.client_id,
+            self.driver_id,
+            self.date_created,
+            self.status,
+        )
+
+
+if __name__ == "__main__":
+    # создание таблиц если они не существуют
+    Base.metadata.create_all(engine)
+
+    # создание новой сессии, для выполнения действий
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # коммит всех изменений
+    session.commit()
+    session.close()
