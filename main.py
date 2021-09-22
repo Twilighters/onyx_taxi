591ece85e79a039f541fc9b02716ae0bd753981d
Twilighters <ilya475@rambler.ru>
2021-09-22 03:36:24 +0300

    fix db bug

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from flask import Flask, jsonify, request
-from onyx_taxi_db import Driver, Client, Order
+from onyx_taxi_db import Driver, Client, Order, engine
+from sqlalchemy.orm import sessionmaker
+from contextlib import contextmanager
+from sqlalchemy.orm import scoped_session
 app = Flask(__name__)
 
+Session = scoped_session(sessionmaker(autoflush=True, autocommit=False, bind=engine))
+
+@contextmanager
+def session_scope():
+        session = Session()
+        try:
+            yield session
+            session.commit()
+        except:
+            session.rollback()
+            raise
+        finally:
+            session.close()
 
 @app.route("/")
 def hello_world():
@@ -11,6 +27,13 @@ def hello_world():
 @app.route("/drivers", methods=["POST"])
 def post_driver():
     content = request.get_json()
+    with session_scope() as session:
+
+        new_driver = Driver(
+            name=content["name"],
+            car=content["car"]
+        )
+        session.add(new_driver)
     return jsonify(content)
 
 
--- onyx_taxi_db.py
+++ onyx_taxi_db.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, Integer, String, create_engine, Boolean, ForeignKey, DateTime, func
+from sqlalchemy import Column, Integer, String, create_engine, Boolean, ForeignKey, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.sql import func
 
 # engine - пул соединений к БД
 engine = create_engine('sqlite:///onyx_taxi.db')
@@ -16,11 +17,11 @@ class Driver(Base):
     # Атрибуты класса описывают колонки таблицы, их типы данных и ограничения
     id = Column(Integer, primary_key=True, autoincrement=True, comment="Идентификатор водителя")
     name = Column(String(25), nullable=False, comment="Имя водителя")
-    car = Column(String, comment="автомобиль водителя")
+    car = Column(String(25), comment="автомобиль водителя")
 
     # переопределение выдачи объекта на экран через print
     def __repr__(self):
-        return "<Player(name={0}, car={1})>".format(self.name, self.car)
+        return "<Driver(name={0}, car={1})>".format(self.name, self.car)
 
 
 class Client(Base):
@@ -43,12 +44,12 @@ class Order(Base):
     id = Column(Integer, primary_key=True, autoincrement=True, comment="Идентификатор заказа")
     address_from = Column(String(50), nullable=False, comment="Адрес: Откуда")
     address_to = Column(String(50), nullable=False, comment="Адрес: Куда")
-    client_id = Column(Integer, ForeignKey('client_id'), nullable=False, comment="Идентификатор клиента")
-    driver_id = Column(Integer, ForeignKey('driver_id'), nullable=False, comment="Идентификатор водителя")
+    client_id = Column(Integer, ForeignKey('clients.id'), nullable=False, comment="Идентификатор клиента")
+    driver_id = Column(Integer, ForeignKey('drivers.id'), nullable=False, comment="Идентификатор водителя")
     date_created = Column(DateTime(timezone=True), default=func.now(), comment="Дата и время создания заказа")
     status = Column(String(20), nullable=False, default="not_accepted", comment="Статус")
-    client = relationship("Client", foreign_keys=[client_id])
-    driver = relationship("Driver", foreign_keys=[driver_id])
+    client = relationship("Client")
+    driver = relationship("Driver")
 
     # переопределение выдачи объекта на экран через print
     def __repr__(self):
